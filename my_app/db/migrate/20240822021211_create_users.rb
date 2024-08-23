4193a5c0bd788e23072773e6e9eaac984306164b
mackaronina <mihamishu123@gmail.com>
2024-08-23 05:40:04 +0300

    aboba

--- my_app/db/migrate/20240822021211_create_users.rb
+++ my_app/db/migrate/20240822021211_create_users.rb
@@ -3,13 +3,10 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password
       t.string :delivery_address
       t.string :phone
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
